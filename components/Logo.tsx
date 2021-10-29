53ef216636197486526b83b1ee312caf232e9868
Rano Hardjosemito <r.hardjo@live.nl>
2021-10-29 23:54:56 +0200

    🩹  Logo redirects to homepage

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,11 +1,14 @@
 import type { NextComponentType } from 'next'
 import Image from 'next/image'
+import Link from 'next/link'
 
 const Logo: NextComponentType = () => {
   return (
-    <div className="bg-white w-10 h-10 md:w-12 md:h-12 rounded-full">
-      <Image src="/shared/logo.svg" alt="Space Logo" width={48} height={48} />
-    </div>
+    <Link href="/">
+      <a className="bg-white w-10 h-10 md:w-12 md:h-12 rounded-full">
+        <Image src="/shared/logo.svg" alt="Space Logo" width={48} height={48} />
+      </a>
+    </Link>
   )
 }
 
