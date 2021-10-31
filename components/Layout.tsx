95d89a0e866a5847983ab352689555650c363639
Rano Hardjosemito <r.hardjo@live.nl>
2021-10-31 20:31:52 +0100

    🔥 Cleanup unused css module

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -6,7 +6,9 @@ const Layout: NextComponentType = ({ children }) => {
   return (
     <div className="min-h-screen max-w-[1440px] mx-auto flex flex-col p-6 md:px-10 lg:px-14 md:py-0 lg:pt-10">
       <Header />
-      <main className="h-full flex-auto lg:pl-28 lg:pr-16">{children}</main>
+      <main className="h-full flex-auto lg:pl-28 lg:pr-16 overflow-hidden">
+        {children}
+      </main>
       <Sidebar />
     </div>
   )
